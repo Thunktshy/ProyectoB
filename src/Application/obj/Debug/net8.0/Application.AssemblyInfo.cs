ffaa6bf76a3649d7058c54c02f90990881836686
Ricardo Flores <tunktshi@gmail.com>
2024-02-24 13:31:21 -0600

    Primer Commit

--- src/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ src/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb6957c15460f34fef8da530a99553f99d0ad9b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
